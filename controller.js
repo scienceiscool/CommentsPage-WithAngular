56a4442ec08c22633d0260c14a088f0902b892ad
Kathy <kathysaad@gmail.com>
2015-05-06 16:28:32 -0700

    Update view and controller

--- controller.js
+++ controller.js
@@ -1,18 +1,29 @@
-"use strict";
-
-var app = angular.module('', []);
+var app = angular.module('CommentsPage', []);
 
 app.controller=('CommentCtrl', function ($scope) {
-	$scope.comments = [
+	$scope.commentsList = [
 		'Hey, I\'m a comment',
 		'Hi, I love comments',
 		'Me too'
 	];
 
-	$scope.add = function (e) { // e for event
-		if (e.which && e.which === 13) { // 13 = Enter
-			$scope.comments.push($scope.newComment);
+	// Note to self: if there's more time, add Enter key functionality
+	$scope.add = function () { // e for event
+		if ($scope.newComment !== null) {
+			$scope.commentsList.push($scope.newComment);
 			$scope.newComment = '';
 		}
+		/*if (e.which && e.which === 13) { // 13 = Enter
+			$scope.commentsList.push($scope.newComment);
+			$scope.newComment = '';
+		} else {*/
+			//if ($scope.newComment !== null) {
+					//$scope.commentsList.push($scope.newComment);
+					//$scope.newComment = '';
+			//}
+		//}
 	};
 });
+
+
+
