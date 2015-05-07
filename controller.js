1a1f632b5fec7e98acc52bc505b033b22edfd6db
Kathy <kathysaad@gmail.com>
2015-05-06 17:16:12 -0700

    Fixed bug in controller.js, I accidently typed = after app.controller

--- controller.js
+++ controller.js
@@ -1,29 +1,31 @@
 var app = angular.module('CommentsPage', []);
 
-app.controller=('CommentCtrl', function ($scope) {
+app.controller('CommentCtrl', function ($scope) {
 	$scope.commentsList = [
 		'Hey, I\'m a comment',
 		'Hi, I love comments',
 		'Me too'
 	];
 
-	// Note to self: if there's more time, add Enter key functionality
-	$scope.add = function () { // e for event
-		if ($scope.newComment !== null) {
+	$scope.add = function () { 
+		if ($scope.newComment !== '') {
 			$scope.commentsList.push($scope.newComment);
 			$scope.newComment = '';
 		}
-		/*if (e.which && e.which === 13) { // 13 = Enter
+	}
+};
+
+/*
+	$scope.add = function ($e) { 
+		if (e.which && e.which === 13) { // 13 = Enter
 			$scope.commentsList.push($scope.newComment);
 			$scope.newComment = '';
-		} else {*/
-			//if ($scope.newComment !== null) {
-					//$scope.commentsList.push($scope.newComment);
-					//$scope.newComment = '';
-			//}
-		//}
+		} else {
+			if ($scope.newComment !== '') {
+					$scope.commentsList.push($scope.newComment);
+					$scope.newComment = '';
+			}
+		}
 	};
 });
-
-
-
+*/
