1c237846df190096a4956c5a6ceb43438aecbfd2
Kathy <kathysaad@gmail.com>
2015-05-06 18:28:16 -0700

    Fix style.css errors and fix bug where user could press + and add a blank comment

--- controller.js
+++ controller.js
@@ -9,9 +9,10 @@ app.controller('CommentCtrl', function ($scope) {
 
 	$scope.add = function (e) { 
 		if ((e.which && e.which === 13) && ($scope.newComment !== '')) { // 13 = Enter key
+			//console.log("hi");
 			$scope.commentsList.push($scope.newComment);
 			$scope.newComment = '';
-		} else {
+		} else if ($scope.newComment !== '') {
 			$scope.commentsList.push($scope.newComment);
 			$scope.newComment = '';
 		}
